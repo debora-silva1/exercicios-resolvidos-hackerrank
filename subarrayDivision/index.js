089a44090703a935851672894fcbab03baecd588
debora-silva <francislayned2@gmail.com>
2024-01-04 13:45:20 -0300

    Resolve exercicio subarrayDivision

---
+++ subarrayDivision/index.js
@@ -0,0 +1,25 @@
+function birthday(s, d, m) {
+    let cont = 0;
+
+    for (let i = 0; i <= s.length - m; i++) {
+        let soma = 0;
+
+        for (let j = i; j < i + m; j++) {
+            console.log(s[j])
+            soma = soma + s[j];
+        }
+
+        if (soma === d) {
+            cont++;
+        }
+    }
+
+    return cont;
+}
+
+const s = [2, 2, 1, 3, 2];
+const d = 4;
+const m = 2;
+
+const result = birthday(s, d, m)
+console.log(result)
